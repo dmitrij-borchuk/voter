6914a57d9da3277b84a97432de3cb2412f214913
dmitrij-borchuk <frunk.lern@gmail.com>
2017-06-28 01:24:20 +0300

    Heaader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppHeader } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeader
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
